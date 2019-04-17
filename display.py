df9d9bb58fb77c377ac6d666f8bf4427bc31c74b
Anders Lauritsen <ducklord@gmail.com>
2019-04-18 00:18:25 +0200

    Made display code more python lib reuse friendly.

--- display.py
+++ display.py
@@ -6,16 +6,18 @@ from time import sleep
 from PIL import Image, ImageDraw, ImageFont, ImageOps
 import epd7in5b
 
-
-"""Main loop starts from here"""
-def main():
+def display(black_image_path, red_image_path):
     epd = epd7in5b.EPD()
     epd.init()
     sleep(5)
-    HBlackimage = Image.open(sys.argv[1])
-    HRedimage = Image.open(sys.argv[2])
-    epd.display(epd.getbuffer(HBlackimage), epd.getbuffer(HRedimage))
+    black_image = Image.open(black_image_path)
+    red_image = Image.open(red_image_path)
+    epd.display(epd.getbuffer(black_image), epd.getbuffer(red_image))
     epd.sleep()
+
+"""Main loop starts from here"""
+def main():
+    display(sys.argv[1], sys.argv[2])
 
 if __name__ == '__main__':
     main()
